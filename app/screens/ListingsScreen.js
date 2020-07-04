e3b0c8f216b3c635a30e9c2054ac72029e79d12e
Mae Kangsadan <maemeier@live.com>
2020-07-04 14:42:29 +0200

    connect api

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
 import colors from "../config/colors";
 import Screen from "../components/Screen";
-
-const listings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg")
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg")
-  }
-];
+import listingApi from "../api/listings";
 
 function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  const loadListings = async () => {
+    const response = await listingApi.getListings();
+    setListings(response.data);
+  };
+  useEffect(() => {
+    loadListings();
+  }, []);
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -30,7 +26,7 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate("ListingDetailsScreen", item)}
           />
         )}
